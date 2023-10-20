5a9d2423b77fb54247eae58eef33f0be95966f2e
Sachini Sathsarani Wijesinghe <it20641092@my.sliit.lk>
2023-10-20 22:36:12 +0530

    parameter tempering in customer elete page

--- admin/customer-delete.php
+++ admin/customer-delete.php
@@ -1,30 +1,48 @@
 <?php require_once('header.php'); ?>
 
 <?php
-if(!isset($_REQUEST['id'])) {
-	header('location: logout.php');
-	exit;
-} else {
-	// Check the id is valid or not
-	$statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id=?");
-	$statement->execute(array($_REQUEST['id']));
-	$total = $statement->rowCount();
-	if( $total == 0 ) {
-		header('location: logout.php');
-		exit;
-	}
-}
-?>
 
-<?php
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    header('location: logout.php');
+    exit;
+}
+
+$id = $_GET['id'];
+
+// Check if the user has permission to delete the customer
+$statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_id = ? LIMIT 1");
+$statement->execute(array($id));
+$customer = $statement->fetch();
+if (!$customer) {
+    header('location: logout.php');
+    exit;
+}
+
 
 	// Delete from tbl_customer
-	$statement = $pdo->prepare("DELETE FROM tbl_customer WHERE cust_id=?");
-	$statement->execute(array($_REQUEST['id']));
+	$statement = $pdo->prepare("DELETE FROM tbl_customer WHERE cust_id = ?");
+	$statement->execute(array($id));
+
 
 	// Delete from tbl_rating
-	$statement = $pdo->prepare("DELETE FROM tbl_rating WHERE cust_id=?");
-	$statement->execute(array($_REQUEST['id']));
+	$statement = $pdo->prepare("DELETE FROM tbl_rating WHERE cust_id = ?");
+	$statement->execute(array($id));
 
 	header('location: customer.php');
+
+
+	/**in this code parameter tempering will prevent by 
+	*Input Validation- The code checks whether the id parameter is set and whether it's a numeric value.
+	* This helps prevent unauthorized access and parameter tampering by ensuring that the id parameter is valid.
+
+
+	*Access Control- The code checks if the user has the permission to delete the customer.
+	 It fetches the customer record associated with the provided id 
+	 and ensures that the customer exists before proceeding 
+	 with the deletion. This additional check helps prevent unauthorized deletion.
+	
+	*Prepared Statements- Prepared statements are used for database queries to
+	 prevent SQL injection attacks.
+	*
+	*/
 ?>
